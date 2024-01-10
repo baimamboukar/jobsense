cd833eb73fb0b6a81f50e19d22e81563bf0e0ada
baimamboukar <baimamboukar@gmail.com>
2024-01-10 17:04:59 +0100

    chore: inject `recent jobs` widget

--- lib/app/features/home/presenation/pages/home_page.dart
+++ lib/app/features/home/presenation/pages/home_page.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
 import 'package:jobsense/app/extensions/contextx.dart';
 import 'package:jobsense/app/extensions/numx.dart';
 import 'package:jobsense/app/extensions/widgetx.dart';
 import 'package:jobsense/app/features/auth/presentation/widgets/overlay_navigation.dart';
+import 'package:jobsense/app/features/home/presenation/widgets/recent_jobs.dart';
 import 'package:jobsense/app/features/home/presenation/widgets/welcome_tile.dart';
 
 @RoutePage()
@@ -45,10 +47,59 @@ class _HomepageState extends State<Homepage> {
           reverseTriggerPercentage: reverseTriggerPercentage,
           child: Column(
             children: [
-              54.vGap,
-              const WelcomeTile(),
+              34.vGap,
+              const WelcomeTile().hPadding,
+              14.vGap,
+              // const Dashboard(),
+
+              const SearchBox().hPadding,
+              24.vGap,
+              const RecentJobs().hPadding,
             ],
-          ).hPadding,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchBox extends StatelessWidget {
+  const SearchBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: context.width,
+      height: 48,
+      decoration: BoxDecoration(
+        color: Colors.grey.withOpacity(.1),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          suffixIcon: GestureDetector(
+            onTap: () async {
+              await showModalBottomSheet<void>(
+                context: context,
+                builder: (context) => BottomSheet(
+                  builder: (context) => Container(
+                    height: 200,
+                  ),
+                  onClosing: () {},
+                ),
+              );
+            },
+            child: const HeroIcon(HeroIcons.adjustmentsHorizontal),
+          ),
+          border: InputBorder.none,
+          hintText: 'Search for jobs, cities or companies',
+          hintStyle: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+          ),
+          prefixIcon: const Icon(Icons.search),
         ),
       ),
     );
