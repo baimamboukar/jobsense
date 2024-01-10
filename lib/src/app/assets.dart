99a430a59afdc3b110bbc9aa954ff2b8d54e1ccf
baimamboukar <baimamboukar@gmail.com>
2024-01-11 00:54:51 +0100

    chore: add and generate `assets`

---
+++ lib/src/app/assets.dart
@@ -0,0 +1,91 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsFontsGilroyBlack
+  /// assets/fonts/Gilroy-Black.ttf
+  static const String assetsFontsGilroyBlack = "assets/fonts/Gilroy-Black.ttf";
+
+  /// Assets for assetsFontsGilroyBold
+  /// assets/fonts/Gilroy-Bold.ttf
+  static const String assetsFontsGilroyBold = "assets/fonts/Gilroy-Bold.ttf";
+
+  /// Assets for assetsFontsGilroyExtraBold
+  /// assets/fonts/Gilroy-ExtraBold.ttf
+  static const String assetsFontsGilroyExtraBold = "assets/fonts/Gilroy-ExtraBold.ttf";
+
+  /// Assets for assetsFontsGilroyLight
+  /// assets/fonts/Gilroy-Light.ttf
+  static const String assetsFontsGilroyLight = "assets/fonts/Gilroy-Light.ttf";
+
+  /// Assets for assetsFontsGilroyMedium
+  /// assets/fonts/Gilroy-Medium.ttf
+  static const String assetsFontsGilroyMedium = "assets/fonts/Gilroy-Medium.ttf";
+
+  /// Assets for assetsFontsGilroyRegular
+  /// assets/fonts/Gilroy-Regular.ttf
+  static const String assetsFontsGilroyRegular = "assets/fonts/Gilroy-Regular.ttf";
+
+  /// Assets for assetsFontsGilroySemiBold
+  /// assets/fonts/Gilroy-SemiBold.ttf
+  static const String assetsFontsGilroySemiBold = "assets/fonts/Gilroy-SemiBold.ttf";
+
+  /// Assets for assetsFontsGilroySemiBoldItalic
+  /// assets/fonts/Gilroy-SemiBoldItalic.ttf
+  static const String assetsFontsGilroySemiBoldItalic = "assets/fonts/Gilroy-SemiBoldItalic.ttf";
+
+  /// Assets for assetsFontsGilroyThin
+  /// assets/fonts/Gilroy-Thin.ttf
+  static const String assetsFontsGilroyThin = "assets/fonts/Gilroy-Thin.ttf";
+
+  /// Assets for assetsFontsGilroyUltraLight
+  /// assets/fonts/Gilroy-UltraLight.ttf
+  static const String assetsFontsGilroyUltraLight = "assets/fonts/Gilroy-UltraLight.ttf";
+
+  /// Assets for assetsImagesBlue
+  /// assets/images/blue.png
+  static const String assetsImagesBlue = "assets/images/blue.png";
+
+  /// Assets for assetsImagesCaasitech
+  /// assets/images/caasitech.jpeg
+  static const String assetsImagesCaasitech = "assets/images/caasitech.jpeg";
+
+  /// Assets for assetsImagesEnchird
+  /// assets/images/enchird.jpeg
+  static const String assetsImagesEnchird = "assets/images/enchird.jpeg";
+
+  /// Assets for assetsImagesEureka
+  /// assets/images/eureka.jpeg
+  static const String assetsImagesEureka = "assets/images/eureka.jpeg";
+
+  /// Assets for assetsImagesIctu
+  /// assets/images/ictu.jpeg
+  static const String assetsImagesIctu = "assets/images/ictu.jpeg";
+
+  /// Assets for assetsImagesMastercard
+  /// assets/images/mastercard.png
+  static const String assetsImagesMastercard = "assets/images/mastercard.png";
+
+  /// Assets for assetsImagesMtn
+  /// assets/images/mtn.png
+  static const String assetsImagesMtn = "assets/images/mtn.png";
+
+  /// Assets for assetsImagesNexttel
+  /// assets/images/nexttel.jpeg
+  static const String assetsImagesNexttel = "assets/images/nexttel.jpeg";
+
+  /// Assets for assetsImagesOrange
+  /// assets/images/orange.png
+  static const String assetsImagesOrange = "assets/images/orange.png";
+
+  /// Assets for assetsImagesSabc
+  /// assets/images/sabc.jpeg
+  static const String assetsImagesSabc = "assets/images/sabc.jpeg";
+
+  /// Assets for assetsImagesUba
+  /// assets/images/uba.png
+  static const String assetsImagesUba = "assets/images/uba.png";
+
+  /// Assets for assetsImagesYango
+  /// assets/images/yango.png
+  static const String assetsImagesYango = "assets/images/yango.png";
+}
